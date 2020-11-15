f4c674ea04fd0e7797987735a5db1f2779e4be82
Gernot Lenkner <g.lenkner@outlook.de>
2020-11-15 09:01:28 +0100

    added comparer functions

--- src/de/elfen20/Card.java
+++ src/de/elfen20/Card.java
@@ -1,5 +1,7 @@
 package de.elfen20;
 
+import java.util.Objects;
+
 class Card {
     public final CardColor CColor;
     public final byte Value;
@@ -17,5 +19,34 @@ class Card {
     @Override
     public String toString() {
         return "[" + CColor + Value + ']';
+    }
+
+    public boolean matchColor(Card card) {
+        if (card == null) return false;
+        return CColor == card.CColor;
+    }
+
+    public boolean matchValue(Card card) {
+        if (card == null) return false;
+        return Value == card.Value;
+    }
+
+    public boolean matchColorOrValue(Card card) {
+        if (card == null) return false;
+        return ((CColor == card.CColor) || Value == card.Value);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        Card card = (Card) o;
+        return Value == card.Value &&
+                CColor == card.CColor;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(CColor, Value);
     }
 }
